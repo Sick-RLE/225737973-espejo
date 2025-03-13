1bf497a790cb3c9e747a1710afe82d835446cbb2
Robin <maanrigar22@gmail.com>
2025-03-13 21:57:29 +0800

    Added Register and Login Blade and usercontroler

--- laravel-espejo-app/routes/web.php
+++ laravel-espejo-app/routes/web.php
@@ -2,8 +2,16 @@
 
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::resource('notes', NoteController::class);
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/register', [UserController::class, 'showRegistrationForm']);
+Route::post('/register', [UserController::class, 'register'])->name('register');
+
+Route::get('/login', [UserController::class, 'showLoginForm']);
+Route::post('/login', [UserController::class, 'login'])->name('login');
+
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
